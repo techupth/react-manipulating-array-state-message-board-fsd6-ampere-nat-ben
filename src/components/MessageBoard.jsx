8d4036fab4e7af3eaa9595bf0fe628c31df802b3
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-06 13:08:50 +0700

    fix(MessageBoard.jsx): fix class to className

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,8 +1,8 @@
 function MessageBoard() {
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
@@ -13,7 +13,7 @@ function MessageBoard() {
         </label>
         <button className="submit-message-button">Submit</button>
       </div>
-      <div class="board">
+      <div className="board">
         <div className="message">
           <h1>Hello all ! This is first message.</h1>
           <button className="delete-button">x</button>
